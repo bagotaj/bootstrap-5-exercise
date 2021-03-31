6ff8294efa8089a19e4474579785167a68b99c7a
bagotaj <bagotaj@gmail.com>
2021-03-31 10:52:24 +0200

    Bootstrap exercise - adding blog and courses parts to the site

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const linkedinEl = document.getElementById('linkedinTooltip');
+const tooltip1 = new bootstrap.Tooltip(linkedinEl);
+
+const twitterEl = document.getElementById('twitterTooltip');
+const tooltip2 = new bootstrap.Tooltip(twitterEl);
+
+const facebookEl = document.getElementById('facebookTooltip');
+const tooltip3 = new bootstrap.Tooltip(facebookEl);
